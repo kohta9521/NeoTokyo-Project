b29c6c496f363687414c610bf9c2a20146cd1b5a
k_95.21 <kohta0421kochi@gmail.com>
2025-01-14 18:58:08 +0900

    add new varcolor setging

---
+++ frontend/src/utils/font.ts
@@ -0,0 +1,9 @@
+// import
+import { Montserrat } from "next/font/google";
+
+// export
+export const montserratFont = Montserrat({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
