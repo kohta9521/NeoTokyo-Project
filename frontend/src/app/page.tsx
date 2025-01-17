4063d34ca7af419c633bc9bf1adf956202d92089
k_95.21 <kohta0421kochi@gmail.com>
2025-01-17 16:21:21 +0900

    add new header and footer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 // components
+import Footer from "@/components/organisms/Footer";
 import Header from "@/components/organisms/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/organisms/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// components
+
+const Footer = () => {
+  return (
+    <div className="w-full h-auto py-10 bg-gray-800">
+      <div className="w-11/12 mx-auto border-2 border-blue-500"></div>
+    </div>
+  );
+};
+
+export default Footer;
